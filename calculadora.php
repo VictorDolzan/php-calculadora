3d924b99de5c994bf65e8de73a14124655a1b909
Victor Dolsan <victor.freitas@mazzafc.tech>
2022-01-13 12:56:46 -0300

    Refatorando o codigo usando a SELECT

--- calculadora.php
+++ calculadora.php
@@ -2,25 +2,25 @@
     $Num1 = $_POST['n1'];
     $Num2 = $_POST['n2'];
 
-    $Op = $_POST['Operador'];
+    $Op = $_POST['operadores'];
     $result;
 
-    if($Op == '+')
+    if($Op == 'soma')
     {
         $result = $Num1 + $Num2;
         echo $result;
     }
-    else if($Op == '-')
+    else if($Op == 'sub')
     {
         $result = $Num1 - $Num2;
         echo $result;
     }
-    else if($Op == '*')
+    else if($Op == 'mult')
     {
         $result = $Num1 * $Num2;
         echo $result;
     }
-    else if($Op == '/')
+    else if($Op == 'div')
     {
         $result = $Num1 / $Num2;
         echo $result;
--- index.php
+++ index.php
@@ -1,17 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculadora</title>
 </head>
+
 <body>
     <form action="calculadora.php" method="POST">
         <input name="n1" type="number" placeholder="Primeiro Numero">
         <input name="n2" type="number" placeholder="Segundo Numero">
-        <input name="Operador" type="text" placeholder="Operador">
+        <select name="operadores" id="1">
+            <option value=""></option>
+            <option value="soma">+</option>
+            <option value="sub">-</option>
+            <option value="div">/</option>
+            <option value="mult">*</option>
+        </select>
         <button type="submit">Calcular</button>
     </form>
 </body>
+
 </html>
